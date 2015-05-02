07956fcde0b1fdb4552c2571a6d28b22a324d77e
Maxim Kolchin <kolchinmax@gmail.com>
2015-05-02 15:10:29 +0300

    Upgraded Jena TDB up to 1.1.0

--- src/main/java/org/deri/cqels/lang/cqels/CQELSParserBase.java
+++ src/main/java/org/deri/cqels/lang/cqels/CQELSParserBase.java
@@ -1,9 +1,8 @@
 package org.deri.cqels.lang.cqels;
 
-import com.hp.hpl.jena.sparql.lang.ParserQueryBase;
+import com.hp.hpl.jena.sparql.lang.SPARQLParserBase;
 
-
-public class CQELSParserBase extends ParserQueryBase
+public class CQELSParserBase extends SPARQLParserBase
     implements CQELSParserConstants
 {
 	public Duration getDuration(String str){
